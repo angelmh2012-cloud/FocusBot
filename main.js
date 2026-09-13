6257f952bcb081d4c321d5dcb953eb653b295f41
angelmh2012-cloud <angelmh2012@gmail.com>
2026-09-12 21:58:24 -0600

    add an svg img and a simplest animation

--- main.js
+++ main.js
@@ -50,27 +50,27 @@ const mouseX = evento.clientX;
 const mouseY = evento.clientY;
 
 const pupila = document.getElementById('pupila')
-const pupila2 = document.getElementById('pupila2')
+// const pupila2 = document.getElementById('pupila2')
 
 
 
 const {x: ojoX, y: ojoY} = centroElemento(pupila);
-const {x: ojoX2, y: ojoY2} = centroElemento(pupila2);
+// const {x: ojoX2, y: ojoY2} = centroElemento(pupila2);
 
 
 const {h: pupilaH, w: pupilaW} = obtenerMedidas('.pupila')
 const {h: eyeH, w: eyeW} = obtenerMedidas('.eye')
 
-const {h: pupilaH2, w: pupilaW2} = obtenerMedidas('.pupila')
-const {h: eyeH2, w: eyeW2} = obtenerMedidas('.eye')
+// const {h: pupilaH2, w: pupilaW2} = obtenerMedidas('.pupila')
+// const {h: eyeH2, w: eyeW2} = obtenerMedidas('.eye')
 
 
 const maxRadio = Math.min((eyeW - pupilaW) / 2, (eyeH - pupilaH) / 2)
-const maxRadio2 = Math.min((eyeW2 - pupilaW2) / 2, (eyeH2 - pupilaH2) / 2)
+// const maxRadio2 = Math.min((eyeW2 - pupilaW2) / 2, (eyeH2 - pupilaH2) / 2)
 
 
 const {x, y} = calcularPupila(mouseX, mouseY, ojoX, ojoY, maxRadio);
-const {x: x2, y: y2} = calcularPupila(mouseX, mouseY, ojoX2, ojoY2, maxRadio2);
+// const {x: x2, y: y2} = calcularPupila(mouseX, mouseY, ojoX2, ojoY2, maxRadio2);
 
 
 transform(pupila, x, y)
